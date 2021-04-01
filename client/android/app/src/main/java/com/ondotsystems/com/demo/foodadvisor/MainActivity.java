377051c3da9256f58e4a67307b65c7d1b439fcf5
Arin <arin@tistory.com>
2021-04-01 14:37:50 +0900

    add capacitor android project

---
+++ client/android/app/src/main/java/com/ondotsystems/com/demo/foodadvisor/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ondotsystems.com.demo.foodadvisor;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+
+    // registerPlugin(PluginInMyApp.class);
+  }
+}
