5f957d247cd83c3f909aba1429b165ad49137246
Arin <arin@tistory.com>
2021-04-01 14:29:34 +0900

    add codepush update

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -9,8 +9,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import RenderView from './RenderView';
 import { GET_FOOTER } from '../../queries';
 import Query from '../../components/Query';
+import { codePush, InstallMode } from 'capacitor-codepush';
+
 
 function App() {
+  codePush.sync({ updateDialog: true, installMode: InstallMode.IMMEDIATE });
+
   return (
     <div>
       <Query query={GET_FOOTER} render={props => <RenderView {...props} />} />
